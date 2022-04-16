05ceb0945846d2fb10922696cbec14665e90745e
AlineCantalice <alinedecantalice@gmail.com>
2022-04-16 19:10:19 -0300

    menu lateral - seleciona usuario e privacidade - tecnicamente completo

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,10 @@ const URL_SERVIDOR = "https://mock-api.driven.com.br/api/v6/uol/";
 const mensagens = [];
 const participantes = [];
 let nome;
+let enviarPara = "Todos";
+let privacidade = "message";
+const listaUsuariosLogados = [];
+let texto;
 
 function entrarNaSala() {
 
@@ -14,7 +18,7 @@ function entrarNaSala() {
     promessa.then(apagarTelaLogin);
     promessa.catch(tratarErros);
 
-    nome = document.querySelector(".input input").value = "";
+    document.querySelector(".input input").value = "";
 }
 
 function atualizarStatus() {
@@ -58,55 +62,124 @@ function renderizarMensagens(response) {
 
 function mensagensDiv(response, conteudo) {
 
-    if (response.type === "message") {
-        conteudo.innerHTML += `<div class="message">
+    if (response.from === nome || response.to === "" || response.to === "Todos" || response.to === nome) {
+
+        if (response.type === "message") {
+            conteudo.innerHTML += `<div class="message">
                         <p>(${response.time})  <strong>${response.from}</strong> para <strong>${response.to}</strong>:  ${response.text}</p>
                     </div>`
-    } else if (response.type === "private_message") {
-        conteudo.innerHTML += `<div class="message ${response.type}">
+        } else if (response.type === "private_message") {
+            conteudo.innerHTML += `<div class="message ${response.type}">
                         <p>(${response.time})  <strong>${response.from}</strong> reservadamente para <strong>${response.to}</strong>:  ${response.text}</p>
                     </div>`
-    } else {
-        conteudo.innerHTML += `<div class="message ${response.type}">
+        } else {
+            conteudo.innerHTML += `<div class="message ${response.type}">
                         <p>(${response.time})  <strong>${response.from}</strong> ${response.text}</p>
                     </div>`
+        }
     }
 
 }
 
 function carregarUsuarios() {
-    const promessa = axios.get(URL_SERVIDOR + "participants");
-    promessa.then(listarUsuarios);
+
+    const menu = document.querySelector(".menu-lateral");
+
+    if (menu.classList.contains("desaparecer")) {
+        const promessa = axios.get(URL_SERVIDOR + "participants");
+        promessa.then(listarUsuarios);
+    }
+    return;
 }
 
 function listarUsuarios(response) {
 
-    let usuario = document.querySelector(".usuarios");
+    const usuario = document.querySelector(".usuarios");
     usuario.innerHTML = "";
 
     for (let i = 0; i < response.data.length; i++) {
-        usuario.innerHTML += `<div class="usuario">
+        if (response.data[i].name !== nome) {
+            usuario.innerHTML += `<div class="usuario" onclick="selecionarUsuario(this)">
                                 <div class="nome-usuario">
                                     <ion-icon name="person-circle"></ion-icon>
-                                    <p>${response.data[i].name}</p>
+                                    <p class="nome-pessoa">${response.data[i].name}</p>
                                 </div>
-                                <ion-icon class="desaparecer" name="checkmark-sharp"></ion-icon>
+                                <ion-icon class="icone-usuario desaparecer" name="checkmark-sharp"></ion-icon>
                             </div>`
+        }
     }
 
 }
 
+function selecionarUsuario(elemento) {
+    const selecionado = document.querySelector(".selecionado");
+
+    if (selecionado !== null && selecionado !== elemento) {
+        elemento.querySelector(".icone-usuario").classList.remove("desaparecer");
+        elemento.classList.add("selecionado");
+        selecionado.classList.remove("selecionado");
+        selecionado.querySelector(".icone-usuario").classList.add("desaparecer");
+    }
+
+    elemento.querySelector(".icone-usuario").classList.remove("desaparecer");
+    elemento.classList.add("selecionado");
+
+    enviarPara = elemento.querySelector(".nome-pessoa").innerHTML;
+    modificarMensagem();
+}
+
+function selecionarVisibilidade(elemento) {
+    const selecionado = document.querySelector(".visibilidade-selecionada");
+
+    if (selecionado !== null && selecionado !== elemento) {
+        elemento.querySelector(".icone").classList.remove("desaparecer");
+        elemento.classList.add("visibilidade-selecionada");
+        selecionado.classList.remove("visibilidade-selecionada");
+        selecionado.querySelector(".icone").classList.add("desaparecer");
+    }
+
+    elemento.querySelector(".icone").classList.remove("desaparecer");
+    elemento.classList.add("visibilidade-selecionada");
+
+    privacidade = elemento.querySelector(".privacidade").innerHTML;
+    modificarMensagem();
+}
+
+function modificarMensagem() {
+    
+    if (privacidade === "message" || privacidade === "Público") {
+        texto = "Público";
+    } else if (privacidade === "private_message" || privacidade === "Reservadamente") {
+        texto = "Reservadamente";
+    }
+
+    document.querySelector(".texto-mensagem p").innerHTML = `Enviando para ${enviarPara} (${texto})`;
+}
+
 function mostrarMenuLateral() {
-    document.querySelector(".caixa-menu-lateral").classList.toggle("desaparecer");
+    document.querySelector(".caixa-menu-lateral").classList.remove("desaparecer");
+    let t = document.querySelector(".menu-lateral").classList.remove("desaparecer");
+}
+
+function sumirMenuLateral(elemento) {
+    let menu = document.querySelector(".menu-lateral");
+    menu.classList.add("desaparecer");
+    elemento.classList.add("desaparecer");
 }
 
 function enviarMensagens() {
 
+    if (privacidade === "Público") {
+        privacidade = "message";
+    } else if (privacidade === "Reservadamente") {
+        privacidade = "private_message";
+    }
+
     let mensagem = {
         from: nome,
-        to: "Todos",
+        to: enviarPara,
         text: document.querySelector(".texto-mensagem input").value,
-        type: "message"
+        type: privacidade
     }
 
     const promessa = axios.post(URL_SERVIDOR + "messages", mensagem);
@@ -116,11 +189,10 @@ function enviarMensagens() {
     document.querySelector(".texto-mensagem input").value = '';
 }
 
-function enviarMensagensEnter(){
+function enviarMensagensEnter() {
     let input = document.querySelector(".texto-mensagem input");
     input.addEventListener("keyup", event => {
-        if(event.keyCode === 13){
-            console.log(event);
+        if (event.keyCode === 13) {
             let btn = document.querySelector(".submit");
             btn.click();
         }
@@ -129,8 +201,8 @@ function enviarMensagensEnter(){
 
 function tratarErros(error) {
     if (error.response.status === 400) {
-        document.querySelector(".aviso").innerHTML = "Já existe um usuário com esse nome!";
-        entrarNaSala();
+        document.querySelector(".aviso").classList.remove("desaparecer");
+        nome = document.querySelector(".input input").value = "";
     }
 }
 
